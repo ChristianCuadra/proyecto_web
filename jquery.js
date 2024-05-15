f7a03105a83340a253cc68db5a35abc811d09215
ignacioCeron <ig.ceron@duocuc.cl>
2024-05-14 22:43:04 -0400

    jquery agregado

---
+++ jquery.js
@@ -0,0 +1,25 @@
+$(function(){
+
+
+    $("#borrar1").click(function(){
+
+        $("#card1").fadeOut();
+
+    });
+
+    $("#borrar2").click(function(){
+
+        $("#card2").hide();
+
+    });
+
+    $("#borrar3").click(function(){
+
+        $("#card3").hide();
+
+    });
+
+
+
+
+});
